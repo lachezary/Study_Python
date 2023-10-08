8ca432230aac496889375740f9a5c0a99a8ffc71
Lachezary <lachezary@gmail.com>
2023-10-08 19:46:21 +0300

    04_06_Swim_Record

---
+++ SoftUni/04_06_Swim_Record.py
@@ -0,0 +1,19 @@
+# world_record = float(input())
+# distance = float(input())
+# speed = float(input())
+
+# delay = (distance / 15) * 12.5
+# delay = round(delay, 2)
+
+# result = speed * distance
+# result = result + delay
+
+# if result < world_record:
+#     print(f"Yes, he succeeded! The new world record is {result:.2f} seconds.")
+# else:
+#     result = result - world_record
+#     print(f"No, he failed! He was {result:.2f} seconds slower.")
+
+result = (3017 / 15) 
+result = result * 12.5 
+print(result)
