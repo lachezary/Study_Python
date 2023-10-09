0d558f3cbb0e1b942aa082c2067ca119f9dec2ac
lachezary <lachezary@gmail.com>
2023-10-09 22:57:46 +0300

    Evening

--- SoftUni/Conditional Statements - Exercise/06_Swim_Record.py
+++ SoftUni/Conditional Statements - Exercise/06_Swim_Record.py
@@ -1,19 +1,19 @@
-# world_record = float(input())
-# distance = float(input())
-# speed = float(input())
+world_record = float(input())
+distance = float(input())
+speed = float(input())
 
-# delay = (distance / 15) * 12.5
-# delay = round(delay, 2)
 
-# result = speed * distance
-# result = result + delay
+result = speed * distance
 
-# if result < world_record:
-#     print(f"Yes, he succeeded! The new world record is {result:.2f} seconds.")
-# else:
-#     result = result - world_record
-#     print(f"No, he failed! He was {result:.2f} seconds slower.")
+intervals = distance // 15
+delay = intervals * 12.5
 
-result = (3017 / 15) 
-result = result * 12.5 
-print(result)
+result = result + delay
+
+
+
+if result < world_record:
+    print(f"Yes, he succeeded! The new world record is {result:.2f} seconds.")
+else:
+    result = result - world_record
+    print(f"No, he failed! He was {result:.2f} seconds slower.")
