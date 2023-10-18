4e988169960e57b8f0f4994c6ab6aad4491a7d12
lachezary <lachezary@gmail.com>
2023-10-18 19:24:48 +0300

    temp

--- Telerik/3_Conditionals Coding Tasks/7_Phone_Bill.py
+++ Telerik/3_Conditionals Coding Tasks/7_Phone_Bill.py
@@ -1,23 +1,3 @@
-# A phone bill plan includes 
-# 1 hour of free calls and 
-# 20 text messages for 12.00 levas. 
-# Each 
-# additional minute costs 0.10 levas and each 
-# additional message costs 0.06 levas. 
-# Also, any additional minutes/text messages are subject to 20% sales tax. 
-# Write a program that calculates the additional charge for text, 
-# the additional charge for minutes, and the sales tax charge for both. 
-# Also, display the total amount paid.
-
-# Input
-# On the first line, you will receive the total amount of text messages.
-# On the second line, you will receive the total amount of minutes.
-# Output
-# 1st line - number of additional messages and additional amount paid
-# 2nd line - number of additional minutes and additional amount paid
-# 3rd line - amount paid in taxes
-# 4th line - total bill
-
 messages_amount = int(input())
 calls_minutes = int(input())
 messages_cost = 0
