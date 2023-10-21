f8c10e0663f3f95fef5034c4d486b81d82f4b71d
lachezary <lachezary@gmail.com>
2023-10-21 12:18:59 +0300

    temp

--- Other/03_07_roots.py
+++ Other/03_07_roots.py
@@ -1,1 +1,13 @@
-#start
+from sre_compile import isstring
+
+
+test_input = input()
+
+try:
+    if test_input.isdecimal():
+        print("decimal")
+    else:
+        print(f"{test_input[0]}")
+
+except IndexError or ValueError:
+    print("Error")
