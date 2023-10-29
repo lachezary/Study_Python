3dc6e5ee128bb4f05017c6559eae34f5afbc9663
lachezary <lachezary@gmail.com>
2023-10-29 16:54:13 +0200

    Home Preparation

---
+++ Other/FreeCodeAcademy/first_lesson.py
@@ -0,0 +1,43 @@
+# phrase = "Learn them Alle"
+# print(len(phrase))
+# print(phrase.count("e", 5))
+# print(3 + 5.5)
+# print("a" * 5)
+# print(3 * (4 + 5))
+# print(10 % 3)
+# num = 5
+# print(str(num) + " my favorite number")
+# num = -5
+# print(abs(num))
+# num = -5
+# print(pow(3, 3))
+# print(3**3)
+# print(max(4, 6))
+# print(min(4, 6))
+# print(round(3.566))
+# from math import *
+# print(floor(5.5553))
+# print(ceil(5.33333))
+# print(sqrt(36))
+
+# name = input("Enter your name: ")
+# age = input("Enter your age: ")
+# print("Hello " + name + "! You are " + age)
+
+
+# num1 = input("End a number: ")
+# num2 = input("End another number: ")
+# result = float(num1) + float(num2)
+# print(result)
+
+# color = input("Choose a color: ")
+# plural_noun = input("Enter a Plural Noun: ")
+# celebrity = input("Enter a celebrity: ")
+# print(f"Roses are {color}")
+# print(f"{plural_noun} are blue")
+# print(f"I love {celebrity}")
+
+friends = ["Kevin", "Karen", "Jim"]
+friends1 = ["Kevin", 1, False]
+
+print(friends)
