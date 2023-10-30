1f4bca78cfd9b95124184780361f286cd2055e8d
Lachezary <lachezary@gmail.com>
2023-10-30 17:44:08 +0200

    lection

--- Other/03_07_roots.py
+++ Other/03_07_roots.py
@@ -1,13 +1,57 @@
-from sre_compile import isstring
+# test_input = input()
+# try:
+#     if test_input.isdecimal():
+#         print("decimal")
+#     else:
+#         print(f"{test_input[0]}")
+
+# except IndexError or ValueError:
+# #     print("Error")
+
+# coordinates = [(4,5), (5,6)]
+# list_coordinates = [4,5]
+# list_coordinates[0] = 55
+
+# print(list_coordinates)
+
+# def say_hi(name, age):
+#     print("Hello " + name + ", you are age " + str(age))
+# # print("Top")
+# # say_hi()
+# # print("Bottom")
+
+# say_hi("Mike", 35)
+# say_hi("Steven", 22)
+
+# ax2 + bx + c = 0
+# a * x ** 2 + b * x + c = 0
+
+# def cube(num):
+#     return num*num*num
+
+    
+# # print(cube(4))
+
+# # print(cube)
+# result = cube(4)
+
+# print(result)
+
+# is_male = False
+# is_tall = False
 
 
-test_input = input()
+# # if is_male or is_tall:
+# #     print("You are a male or tall or both")
+# # else:
+# #     print("You neither male nor tall")
+# if is_male and is_tall:
+#     print("You are a tall male")
+# elif is_male and not(is_tall):
+#     print("You are a short male")  
+# elif not(is_male) and is_tall:
+#     print("You are not a male, but you are tall")
+# else:
+#     print("You neither male nor tall")
 
-try:
-    if test_input.isdecimal():
-        print("decimal")
-    else:
-        print(f"{test_input[0]}")
 
-except IndexError or ValueError:
-    print("Error")
