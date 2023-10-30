fb13d740095fcf3641a546ffa4ce15380b2bff71
lachezary <lachezary@gmail.com>
2023-10-30 20:34:13 +0200

    free code

--- Other/FreeCodeAcademy/first_lesson.py
+++ Other/FreeCodeAcademy/first_lesson.py
@@ -61,11 +61,52 @@
 # # print(friends.index("Jim"))
 # # print(friends)
 # friends2 = friends.copy()
-# print(friends2)
+# # print(friends2)
 
+# name = input()
+# print(name[1])
+# coordinates = [(4,5), (5,6)]
+# list_coordinates = [4,5]
+# list_coordinates[0] = 55
 
-name = input()
+# print(list_coordinates)
 
+# def say_hi(name, age):
+#     print("Hello " + name + ", you are age " + str(age))
+# # print("Top")
+# # say_hi()
+# # print("Bottom")
 
-print(name[1])
+# say_hi("Mike", 35)
+# say_hi("Steven", 22)
+
+# ax2 + bx + c = 0
+# a * x ** 2 + b * x + c = 0
+
+# def cube(num):
+#     return num*num*num
+
+    
+# # print(cube(4))
+
+# # print(cube)
+# result = cube(4)
+
+# print(result)
+
+# is_male = False
+# is_tall = False
+
+# # if is_male or is_tall:
+# #     print("You are a male or tall or both")
+# # else:
+# #     print("You neither male nor tall")
+# if is_male and is_tall:
+#     print("You are a tall male")
+# elif is_male and not(is_tall):
+#     print("You are a short male")  
+# elif not(is_male) and is_tall:
+#     print("You are not a male, but you are tall")
+# else:
+#     print("You neither male nor tall")
 
