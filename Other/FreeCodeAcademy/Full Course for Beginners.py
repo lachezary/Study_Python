c294efa6e5550fd36dc92c95c02c7dd7c66b8221
Lachezary <lachezary@gmail.com>
2023-10-31 17:46:47 +0200

    homework

--- Other/FreeCodeAcademy/Full Course for Beginners.py
+++ Other/FreeCodeAcademy/Full Course for Beginners.py
@@ -158,7 +158,65 @@
 # }
 # print(monthConversions["Jan"])
 # print(monthConversions.get("Feb"))
-# print(monthConversions.get("6"))
+# print(monthConversions.get(6))
 # print(monthConversions.get("LLL", "Not a valid key"))
 
+# i = 1
+
+# while i <= 10:
+#     print(i)
+#     i += 1
+    
+# print("Done with loop")
+
+# secret_word = "giraffe"
+# guess = ""
+# guess_counter = 0
+# guess_limit = 3
+# out_of_guesses = False
+
+# while guess != secret_word and not(out_of_guesses):
+#     if guess_counter < guess_limit:
+#         guess = input("Enter guess: ")
+#         guess_counter += 1
+#     else:
+#         out_of_guesses = True
+
+# if out_of_guesses:
+#     print("Out of guesses. You lose!")
+# else:
+#     print("You win")
+    
+# # print("You win")
+
+
+# for letter in "Giraffe Academy":
+#     print(letter)
+
+# friends = ["Jim", "Karen", "Kevin"]
+
+# # for friend in friends:
+# #     print(friend)
+
+# # for i in range (3,10):
+# #     print(i)
+
+
+# for index in range (len(friends)):
+# #     print(friends[index])
+
+# for index in range (5):
+#     if index == 0:
+#         print("first iteration")
+#     else:
+#         print("not first iteration")
+        
+# print(2**3)        
+def raise_to_power(base_num, power_num):
+    result = 1
+    for index in range (power_num):
+        result = result * base_num
+    return result
+
+print(raise_to_power(3,6))
 
