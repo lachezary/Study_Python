cc43cfd8fb1a6f18a2a34f43e4b94f54ec09d334
lachezary <lachezary@gmail.com>
2023-11-13 21:06:17 +0200

    homework

--- Telerik/Mock_Exam/Crooked Digits.py
+++ Telerik/Mock_Exam/Crooked Digits.py
@@ -7,14 +7,17 @@ number = number.replace("-", "")
 length = len(number)
 total = 0
 
-while length >= 2:
-    total = 0
-    for i in range(length):
-        digit = int(number[i])
-        total += digit
-    number = str(total)
-    length = len(number)
-    if total < 10:
-        break
+if length == 1:
+    total = number
+else:
+    while length >= 2:
+        total = 0
+        for i in range(length):
+            digit = int(number[i])
+            total += digit
+        number = str(total)
+        length = len(number)
+        if total < 10:
+            break
 
 print(total)
--- Telerik/Mock_Exam/Prime Triangle.py
+++ Telerik/Mock_Exam/Prime Triangle.py
@@ -1,12 +1,20 @@
-num = int(input())
 
-for i in range(1, num + 1):
-    for prime in range(2, i):
-        if i % prime == 0:
-            print(0, end="")
-            break
-    else:
-        print(1, end="")
-    if i < num:
-        print(" ", end="")
-print()
+
+
+
+
+
+
+input_num = int(input())
+
+is_prime = True
+
+for prime_tester in range (1, input_num+1):
+    if input_num % prime_tester == 0:
+        if prime_tester == 1 or prime_tester == input_num:
+            continue
+        else:
+            is_prime = False
+
+                
+print("prime" if is_prime else "not prime")
