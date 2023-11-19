47767be9fd30c21d5b44aab57abd22789d86a78e
lachezary <lachezary@gmail.com>
2023-11-19 19:57:15 +0200

    exam preparation

--- Telerik/Mock_Exam/Game.py
+++ Telerik/Mock_Exam/Game.py
@@ -1,24 +1,25 @@
-# Input
-# Read from the standard input
-
-# The first line of the input will be positive three-digit number N.
-# Output
-# Print on the standard output
-
-# The result should be the calculated biggest number.
-# The calculation order
-
-
 num = input()
+total = 0
 
 num1 = int(num[0])
 num2 = int(num[1])
 num3 = int(num[2])
-total = 0
-total = 0
 
-# for i in range (3):
-#     if num1 =
-
-
+if num.count("1") >= 2 or num.count("0") >= 2:
+    total = num1 + num2 + num3
+elif num.count("1") == 1 and num.count("0") == 1:
+    total = num1 + num2 + num3
+    
+elif num.count("1") == 1 or num.count("0") == 1:
+    if num1 <= 1:
+        total = num2 * num3 + num1
+    elif num2 <= 1:
+        total = num3 * num1 + num2
+    elif num3 <= 1:
+        total = num2 * num1 + num3
+else:
+    total = num1 * num2 * num3
+    
+    
+    
 print(total)
